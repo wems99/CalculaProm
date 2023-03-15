fe499149fddb3e9fc9df9c2b1aac82d03ffb2996
Di Carlo Martínez <67888776+dicarlomtz@users.noreply.github.com>
2023-03-14 19:13:13 -0600

    Update Validator.kt

--- app/src/main/java/com/example/myapplication/utils/validator/Validator.kt
+++ app/src/main/java/com/example/myapplication/utils/validator/Validator.kt
@@ -1,17 +1,19 @@
 package com.example.myapplication.utils.validator
 
-fun isInRange(minimum: Int, maximum: Int, value: Int): Boolean {
-    if (value < minimum || value > maximum) {
-        return false;
-    }
-
-    return true;
-}
-
+/**
+ * Determines if a string is defined and not empty
+ * @param value string to be evaluated
+ * @return true if string defined and not empty, false if not
+ */
 fun isStringNotEmpty(value: String?): Boolean {
     return !value.isNullOrEmpty();
 }
 
+/**
+ * Determines if a list of strings are defined and not empty
+ * @param values list of strings to be evaluated
+ * @return true if each element on the list is defined and not empty, false if not
+ */
 fun areStringsNotEmpty(vararg values: String?): Boolean {
     for (value in values) {
         if (!isStringNotEmpty(value)) {
@@ -20,4 +22,14 @@ fun areStringsNotEmpty(vararg values: String?): Boolean {
     }
 
     return true;
+}
+
+/**
+ * Determines if a string contains digits
+ * @param value the text to be evaluated
+ * @return false if contains digits, true if not
+ */
+fun containsDigits(value: String): Boolean {
+    val regex = Regex("\\d+")
+    return regex.containsMatchIn(value)
 }
